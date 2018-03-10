ec863226fdcfc3e6e3820b2b60ec102f164d1401
Takayuki YATO <tak_yato@yahoo.co.jp>
2018-03-11 08:48:06 +0900

    wynhotep v0.3.1

--- wynhotep/wynhotep.pl
+++ wynhotep/wynhotep.pl
@@ -1,21 +1,29 @@
 #!/usr/bin/perl
 use v5.12;
 use File::Copy 'copy';
+use Cwd 'getcwd';
 use Time::HiRes 'sleep';
 my $program = "wynhotep";
-my $version = "0.2.0";
+my $version = "0.3.1";
 
 my $tempb = "__whtp";
+my $windows = ($^O eq 'MSWin32');
 
 sub main {
   if ($ENV{WYNHOTEP_FOOLYSH_DIR} ne '') {
-    main_foolysh();
+    foolysh_main();
   } elsif ($ENV{WYNHOTEP_DOCKER_IMAGE} ne '') {
-    main_docker();
+    if ($ENV{WYNHOTEP_DOCKER_USE_PIPE} ne '') {
+      dockerwh_main();
+    } else {
+      docker_main();
+    }
   } else {
-    my $opt = shift(@ARGV) // "";
-    if ($opt =~ m/^--?h(?:elp)?$/) {
+    my $opt = shift(@ARGV) // '';
+    if ($opt eq '-help' || $opt eq '--help') {
       show_usage();
+    } elsif ($opt eq '-v' || $opt eq '--version') {
+      say("$program version $version");
     } else {
       error("not properly set up (try '--help')");
     }
@@ -51,6 +59,9 @@ sub write_whole {
 sub info {
   say STDERR (join(": ", $program, @_));
 }
+sub panic {
+  info("PANIC(@_)"); exit(-2);
+}
 sub error {
   info(@_); exit(-1);
 }
@@ -58,25 +69,29 @@ sub error {
 #-----------------------------------------------------------
 # Foolysh
 
-my $foolysh_dir = "C:/Users/yato/Repos/mysatisfi/foolysh";
-my ($wait_intv, $wait_limit) = (0.5, 600);
+use constant {
+  FOOLYSH_WAIT_INTV => 0.5,
+  FOOLYSH_WAIT_LIMIT => 600,
+};
 
-sub main_foolysh {
+my $foolysh_dir;
+
+sub foolysh_main {
   $foolysh_dir = $ENV{WYNHOTEP_FOOLYSH_DIR};
   (-d $foolysh_dir) or error("no such directory", $foolysh_dir);
   my $name = "whtp$$";
   my $dir = "$foolysh_dir/$name";
   my $sh = "$foolysh_dir/$name.sh";
   mkdir($dir) or error("failure", $dir);
-  export_files($dir);
+  foolysh_export($dir);
 
   write_whole($sh, "satysfi @ARGV");
-  for (my $w = 0; $w < $wait_limit; $w += $wait_intv) {
-    sleep($wait_intv);
+  for (my $w = 0; $w < FOOLYSH_WAIT_LIMIT; $w += FOOLYSH_WAIT_INTV) {
+    sleep(FOOLYSH_WAIT_INTV);
     (! -f $sh) and last;
   }
   (! -f $sh) or error("failure in foolysh");
-  import_files($dir);
+  foolysh_import($dir);
   my $out = read_whole("$dir/__stdout");
   my $err = read_whole("$dir/__stderr");
   unlink($sh, glob("$dir/*"));
@@ -86,14 +101,14 @@ sub main_foolysh {
   print($out);
 }
 
-sub export_files {
+sub foolysh_export {
   my ($dir) = @_;
   foreach my $f (glob("*.*")) {
     copy($f, "$dir/$f");
   }
 }
 
-sub import_files {
+sub foolysh_import {
   my ($dir) = @_;
   foreach my $f (glob("$dir/*.pdf")) {
     my $ff = $f =~ s|.*/||r;
@@ -102,108 +117,201 @@ sub import_files {
 }
 
 #-----------------------------------------------------------
-# Docker
+# Docker helpers
 
-my $image_name = "test/satysfi";
-my $docker = "docker";
+use constant DOCKER => 'docker';
 
-sub main_docker {
-  $image_name = $ENV{WYNHOTEP_DOCKER_IMAGE};
-  (check_docker()) or error("Docker is not working");
-  (check_image()) or build_image();
+my $docker = DOCKER;
 
-  my $input = make_input();
-  my $output = run_container($input);
-  make_output($output);
-}
-
-sub make_input {
-  my $tar = "$tempb-in.tar.gz";
-  unlink($tar);
-  write_whole("probe.whtp", "SATySFi");
-  local $_ = `tar cfz $tar *.saty *.satyh probe.whtp 2>$tempb.err`;
-  unlink("probe.whtp", "$tempb.err");
-  (-s $tar) or error("failure in tar");
-  return $tar;
-}
-
-sub make_output {
-  my ($tar) = @_;
-  if (-s $tar) { # output files exist
-    local $_ = `tar xfz $tar 2>$tempb.err`;
-    ($? == 0) or error("failure in tar");
-    unlink("$tempb.err");
-  }
-  unlink($tar);
-}
-
-sub check_docker {
-  local $_ = `$docker ps`;
-  return ($? == 0);
-}
-
-sub check_image {
+sub docker_check_image {
+  my ($image) = @_;
   local $_ = `$docker images`;
-  ($? == 0) or docker_error();
+  ($? == 0) or error("Docker is not working");
   foreach (split(m/\n/, $_)) {
     my $id = (split(m/\s+/, $_, 2))[0];
-    ($id eq $image_name) and return 1;
+    ($id eq $image) and return 1;
   }
   return;
 }
 
-sub run_container {
-  my ($input) = @_;
-  my $output = "$tempb-out.tar.gz";
-  local $_ = `$docker run --rm -i $image_name @ARGV <$input >$output`;
-  ($? == 0) or docker_error();
-  (-f $output) or error("ERR(1)");
-  unlink($input);
-  return $output;
+sub docker_get_workdir {
+  my ($image) = @_;
+  local $_ = `$docker run --rm $image pwd`; chomp($_);
+  ($? == 0 && $_ ne '') or docker_error();
+  return $_;
 }
 
-sub build_image {
-  info("build docker image", $image_name);
-  my ($dockerfile, $script) = build_source();
-  mkdir("$tempb-build") or error("cannot create directory");
-  chdir("$tempb-build") or error("ERR(1)");
-  write_whole("Dockerfile", $dockerfile);
-  write_whole("run-satysfi.sh", $script);
-  system("$docker build -t $image_name .");
-  ($? == 0) or docker_error();
-  (-f "Dockerfile") and unlink("Dockerfile", glob("*.*"));
-  chdir("..");
-  rmdir("$tempb-build") or error("cannot remove directory");
+sub docker_get_user {
+  my ($image) = @_;
+  local $_ = `$docker run --rm $image id -u -n`; chomp($_);
+  ($? == 0 && $_ ne '') or docker_error();
+  return $_;
+}
+
+sub docker_get_hostdir {
+  local $_ = getcwd();
+  if ($windows) {
+    if (m|^\w:/|ai) {
+      substr($_, 0, 2) = lc(substr($_, 0, 1)) . '/';
+    } else {
+      error("cannot use the current dicctory", $_);
+    }
+  }
+  return $_;
 }
 
 sub docker_error {
   info("DOCKER ERROR"); exit(-1);
 }
 
-sub build_source {
-  return (<<'EOT1', <<'EOT2');
-FROM pandaman64/satysfi
-USER opam
-RUN mkdir -p /home/opam/scripts && mkdir -p /home/opam/work
-WORKDIR /home/opam/work
-ADD run-satysfi.sh /home/opam/scripts/
-ENTRYPOINT ["/home/opam/scripts/run-satysfi.sh"]
-EOT1
+#-----------------------------------------------------------
+# Docker
+
+use constant {
+  DOCKER_DEFAULT_WORKDIR => '/tmp',
+};
+
+my $docker_image;
+
+sub docker_main {
+  $docker_image = $ENV{WYNHOTEP_DOCKER_IMAGE};
+  if (!docker_check_image($docker_image)) {
+    if ($ARGV[0] ne '--version') {
+      print STDERR ("Docker image '$docker_image' is yet not loaded.\n");
+      print STDERR ("Hit Enter to start loading...");
+      <STDIN>;
+    }
+    system("$docker pull $docker_image");
+    ($? == 0) or docker_error();
+  }
+
+  my $workdir = DOCKER_DEFAULT_WORKDIR;
+  local $_ = $ENV{WYNHOTEP_DOCKER_WORKDIR};
+  ($_ ne '') and $workdir = docker_resolve_workdir($_);
+
+  my $hostdir = docker_get_hostdir();
+  system("$docker run -it --rm -v $hostdir:$workdir -w $workdir $docker_image"
+   . " satysfi @ARGV");
+
+  exit($? >> 8);
+}
+
+sub docker_resolve_workdir {
+  local ($_) = @_;
+  (m|^/|) and return $_;
+  my $wd = docker_get_workdir($docker_image);
+  s|/$||; s|^\.$||;
+  return $wd . (($_ eq '') ? '' : "/$_");
+}
+
+#-----------------------------------------------------------
+# DockerWH (use pipe)
+
+use constant {
+  DOCKERWH_IMAGE_NAME => 'zr-tex8r/wynhotep',
+};
+
+my $docker_base_image;
+
+sub dockerwh_main {
+  $docker_base_image = $ENV{WYNHOTEP_DOCKER_IMAGE};
+  $docker_image = DOCKERWH_IMAGE_NAME;
+  if (!docker_check_image($docker_image)) {
+    if ($ARGV[0] ne '--version') {
+      print STDERR ("Docker image '$docker_image' is yet not built.\n");
+      print STDERR ("Hit Enter to start building...");
+      <STDIN>;
+    }
+    dockerwh_build_image();
+  }
+
+  my $input = dockerwh_make_input();
+  my $output = dockerwh_run_container($input);
+  dockerwh_make_output($output);
+}
+
+sub dockerwh_make_input {
+  my $tar = "$tempb-in.tar.gz";
+  my $probe = "probe.whtp";
+  unlink($tar);
+  write_whole($probe, "SATySFi");
+  system("tar cfz $tar *.* 1>$tempb.out 2>$tempb.err");
+  ($? == 0 && -s $tar) or error("failure in tar");
+  unlink($probe, "$tempb.out", "$tempb.err");
+  return $tar;
+}
+
+sub dockerwh_make_output {
+  my ($tar) = @_;
+  if (-s $tar) { # output files exist
+    system("tar xfz $tar 1>$tempb.out 2>$tempb.err");
+    #($? == 0) or error("failure in tar");
+    unlink("$tempb.out", "$tempb.err");
+  }
+  unlink($tar);
+}
+
+sub dockerwh_run_container {
+  my ($in) = @_;
+  my $out = "$tempb-out.tar.gz";
+  system("$docker run --rm -i $docker_image @ARGV <$in >$out");
+  ($? == 0 && -f $out) or docker_error();
+  unlink($in);
+  return $out;
+}
+
+sub dockerwh_build_image {
+  if (!docker_check_image($docker_base_image)) {
+    info("pull docker base image", $docker_base_image);
+    system("$docker pull $docker_base_image");
+    ($? == 0) or docker_error();
+  }
+  info("build docker image", $docker_image);
+  my $user = docker_get_user($docker_base_image);
+  my $wd = docker_get_workdir($docker_base_image);
+  my ($dockerfile, $script) = dockerwh_build_source($user, $wd);
+  mkdir("$tempb-build") or error("cannot create directory");
+  chdir("$tempb-build") or panic();
+  write_whole("Dockerfile", $dockerfile);
+  write_whole("run-satysfi.sh", $script);
+  system("$docker build -t $docker_image .");
+  ($? == 0) or docker_error();
+  (-f "Dockerfile") and unlink("Dockerfile", glob("*.*"));
+  chdir("..") or panic();
+  rmdir("$tempb-build") or error("cannot remove directory");
+}
+
+sub dockerwh_build_source {
+  my ($user, $workdir) = @_;
+  my $dockerfile = <<"EOT";
+FROM $docker_base_image
+ADD run-satysfi.sh /tmp/
+USER $user
+RUN mkdir -p $workdir/_scripts && mkdir -p $workdir/_work \\
+  && cp /tmp/run-satysfi.sh $workdir/_scripts \\
+  && chmod +x $workdir/_scripts/run-satysfi.sh
+WORKDIR $workdir/_work
+ENTRYPOINT ["$workdir/_scripts/run-satysfi.sh"]
+EOT
+  my $script =  <<"EOT";
 #!/bin/bash
+if [ -f ~/.profile ]; then
+  . ~/.profile || true
+fi
 # receive input files
-cat - | tar xfz - >/dev/null
-# opam init
-. /home/opam/.opam/opam-init/init.sh > /dev/null 2>&1 || true
+cat - | tar xfz - >/dev/null 2>&1
 # do it
-satysfi $* 1>&2
+satysfi \$* 1>&2
 if ls *.pdf >/dev/null 2>&1
 then
   # send output files
-  tar cfz __out.tar.gz *.pdf >/dev/null 2>&1
+  tar cfz __out.tar.gz *.pdf *.satysfi-aux >/dev/null 2>&1
   cat __out.tar.gz
 fi
-EOT2
+EOT
+  return ($dockerfile, $script);
 }
 
+#-----------------------------------------------------------
 main();
 # EOF
